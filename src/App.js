0786221cdcacaba540be2237ca62a46f5cc56ba2
chochiniz <chochinize@gmail.com>
2021-11-23 23:58:08 +0100

    add voice

--- src/App.js
+++ src/App.js
@@ -1,62 +1,73 @@
-import './App.css';
-import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom'
-import Movement from './Components/Movement';
-import Users from './Components/users'
-import Login from './Components/login';
+import "./index.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Movement from "./Components/Movement";
 
-
+import Login from "./Components/Login";
+import Home from "./Components/Home";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  
-  
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      const token = localStorage.getItem("tokenStore");
+      if (token) {
+        const verified = await axios.get("/users/verify", {
+          headers: { Authorization: token },
+        });
+        console.log(verified);
+        setIsLogin(verified.data);
+        if (verified.data === false) return localStorage.clear();
+      } else {
+        setIsLogin(false);
+      }
+    };
+    checkLogin();
+  }, []);
 
   return (
- 
-    
     <Router>
-        
-    <header className=''>
-   
-          <ul className='flex border-red-600 border-2  max-w-screen flex-row-reverse z-2'>
-            <li className='p-2'>
-              <Link to="/">Home</Link>
-            
-            </li>
-            <li className='p-2'>
+      <header className="h-max  w-full z-40 bg-gradient-to-t from-blue-300 via-black to-black">
+        <ul className="flex border-white border-4  border-opacity-20 max-w-screen flex-row-reverse ">
+          <li className="p-2">
+            <Link to="/" className="text-white fancy-link font-anton">
+              Home
+            </Link>
+          </li>
+          <li className="p-2">
+            <Link
+              to="/projects/api/projects"
+              className="text-white fancy-link font-anton "
+            >
+              Projects
+            </Link>
+          </li>
 
-              <Link to="/projects/api/projects">Projects</Link>
-            </li>
-            <li className='p-2'>
+          {/* <li className='p-2'>
+              <Link to="/Login" className='text-white fancy-link font-anton'>Login</Link>
+            </li> */}
+          <li className="p-2">
+            <Link to="/SignUp" className="text-white fancy-link font-anton">
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      </header>
 
-              <Link to="/login">Log in</Link>
-            </li>
-     
-         
-            </ul>
-  
-          
-    </header>
-
-
-        <Switch>
-          <Route path="/projects/api/projects">
-       
+      <Switch>
+        <Route path="/projects/api/projects">
           <Movement />
-         
-           
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/">
-         
-          </Route>
-        </Switch>
-      
-  
+        </Route>
+        <Route path="/Login">
+          <Login setIsLogIn={(isLogin, setIsLogin)} />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </Router>
-      
-
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
    darkMode: false, // or 'media' or 'class'
    theme: {
+    fontFamily:{
+            anton:['Anton', 'sans-serif'],
+            Tenali:['Tenali Ramakrishna', 'sans-serif'],
+          },
      extend: {},
    },
    variants: {
@@ -10,3 +14,30 @@ module.exports = {
    },
    plugins: [],
  }
+
+// module.exports = {
+//   mode: '',
+//   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './Components/**/*.{js,ts,jsx,tsx}'],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     fontFamily:{
+//       anton:['Anton', 'sans-serif'],
+//       Tenali:['Tenali Ramakrishna', 'sans-serif'],
+//     },
+//     extend: {
+//       keyframes: {
+//         wiggle: {
+//           "0%, 100%": { transform: "rotate(-3deg)" },
+//           "50%": { transform: "rotate(3deg)" }
+//         }
+//       },
+//       animation: {
+//         wiggle: "wiggle 200ms ease-in-out"
+//       }
+//     },
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
